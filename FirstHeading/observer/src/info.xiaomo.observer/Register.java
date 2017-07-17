1a1d451c567a90378401e08fd7c4cb7baa266633
xiaomo <xiaomo@xiaomo.info>
2017-07-17 14:30:50 +0800

    观察者模式

--- FirstHeading/observer/src/info.xiaomo.observer/Register.java
+++ FirstHeading/observer/src/info.xiaomo.observer/Register.java
@@ -11,21 +11,15 @@ import info.xiaomo.observer.display.impl.CurrentConditionsDisplay;
  * <p>
  * author: xiaomo
  * github: https://github.com/xiaomoinfo
- * email : xiaomo@xiamoo.info
+ * email : xiaomo@xiaomo.info
  * QQ    : 83387856
- * Date  : 17/6/15 10:36
+ * Date  : 2017/7/17 14:26
  * desc  :
  * Copyright(©) 2017 by xiaomo.
  */
-public class ObserverMain {
-    public static void main(String[] args) {
-        // 获得一个气象站数据实例
-        WeatherData weatherData = new WeatherData();
-        // 注册观察者
-        Register.register(weatherData);
-        // 数据发生变化
-        weatherData.setMeasurements(10,20,30);
+public class Register {
 
+    public static void register (WeatherData weatherData){
+        weatherData.registerObserver(new CurrentConditionsDisplay(weatherData));
     }
-
 }
