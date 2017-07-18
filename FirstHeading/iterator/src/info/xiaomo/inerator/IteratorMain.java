9a1b525bbba190f4ece5813f3359e70e6a2d923b
xiaomo <xiaomo@xiaomo.info>
2017-07-18 13:33:07 +0800

    迭代器模式

---
+++ FirstHeading/iterator/src/info/xiaomo/inerator/IteratorMain.java
@@ -0,0 +1,21 @@
+package info.xiaomo.inerator;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/7/18 13:31
+ * desc  : 迭代器模式
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class IteratorMain {
+    public static void main(String[] args) {
+
+    }
+}
