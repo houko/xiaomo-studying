d06ac3102c904c27e0591e0b79b520b970516b17
xiaomo <xiaomo@xiaomo.info>
2017-07-18 13:04:12 +0800

    模板模式

---
+++ FirstHeading/templateMethod/src/info/xiaomo/template/bean/Coffee.java
@@ -0,0 +1,29 @@
+package info.xiaomo.template.bean;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/7/17 20:53
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Coffee  extends CaffeineBeverage{
+    @Override
+    void brew() {
+        System.out.println("用热水泡咖啡");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("添加糖和牛奶");
+    }
+
+
+}
---
+++ FirstHeading/templateMethod/src/info/xiaomo/template/bean/Tea.java
@@ -0,0 +1,28 @@
+package info.xiaomo.template.bean;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/7/17 20:53
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Tea extends CaffeineBeverage {
+
+    @Override
+    void brew() {
+        System.out.println("用热水泡茶");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("添加柠檬");
+    }
+}
