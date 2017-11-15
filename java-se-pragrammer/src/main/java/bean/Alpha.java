6bb40f84ded5f0e445efb6e0e88bf326629e9509
xiaomo <xiaomo@xiaomo.info>
2017-11-15 21:03:28 +0800

    q20

--- java-se-pragrammer/src/main/java/bean/Alpha.java
+++ java-se-pragrammer/src/main/java/bean/Alpha.java
@@ -1,3 +1,5 @@
+package bean;
+
 /**
  * 把今天最好的表现当作明天最新的起点．．～
  * いま 最高の表現 として 明日最新の始発．．～
@@ -7,15 +9,23 @@
  * github  : https://github.com/xiaomoinfo
  * email   : xiaomo@xiaomo.info
  * QQ      : 83387856
- * Date    : 2017/11/15 20:37
+ * Date    : 2017/11/15 21:00
  * desc    :
  * Copyright(©) 2017 by xiaomo.
  */
-public class Question10 {
+public class Alpha {
+    static int s;
+    int ns;
 
-
-
-    public static void main(String[] args) {
-
+    public Alpha(int ns) {
+        if (s < ns) {
+            s = ns;
+            this.ns = ns;
+        }
     }
+
+    public void doPrint() {
+        System.out.println("ns = " + ns + "  s = " + s);
+    }
+
 }
