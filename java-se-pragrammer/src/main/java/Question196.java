7b9e4f3641b98f912a61b13af8c5c8cdc8a1b8f7
xiaomo <xiaomo@xiaomo.info>
2017-11-21 08:37:38 +0800

    197

---
+++ java-se-pragrammer/src/main/java/Question196.java
@@ -0,0 +1,27 @@
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ *
+ * @author : xiaomo
+ * github  : https://github.com/xiaomoinfo
+ * email   : xiaomo@xiaomo.info
+ * QQ      : 83387856
+ * Date    : 2017/11/21 8:34
+ * desc    :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question196 {
+    abstract class X{
+        public abstract void methodX();
+    }
+
+    interface Y{
+        public void methodY();
+    }
+
+     abstract class Z extends X implements Y{
+
+
+    }
+}
