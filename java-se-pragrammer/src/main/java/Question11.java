8b409fe17925d4dc8ca231c133f7fdd027e11402
xiaomo <xiaomo@xiaomo.info>
2017-11-21 17:59:28 +0800

    question

--- java-se-pragrammer/src/main/java/Question11.java
+++ java-se-pragrammer/src/main/java/Question11.java
@@ -7,20 +7,20 @@
  * github  : https://github.com/xiaomoinfo
  * email   : xiaomo@xiaomo.info
  * QQ      : 83387856
- * Date    : 2017/11/15 19:58
+ * Date    : 2017/11/21 16:48
  * desc    :
  * Copyright(©) 2017 by xiaomo.
  */
-public class Question7 {
+public class Question11 {
     public static void main(String[] args) {
-        int[] lst = {1, 2, 3, 4, 5, 4, 3, 2, 1};
-        int sum = 0;
-        for (int frnt = 0, real = lst.length - 1; frnt < 5 && real >= 5; ) {
-            System.out.println("frnt :" + frnt + " real:" + real);
-            sum = sum + lst[frnt] + lst[real];
-            frnt++;
-            real--;
+        int row = 10;
+        for (; row > 0; ) {
+            int col = row;
+            while (col >= 0) {
+                System.out.print(col + " ");
+                col -= 2;
+            }
+            row = row / col;
         }
-        System.out.println(sum);   // 20
     }
 }
