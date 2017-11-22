e04ca087afabbdb81402479969d2b45db1602071
xiaomo <xiaomo@xiaomo.info>
2017-11-22 17:41:06 +0800

    主函数调用主函数

---
+++ java-se-pragrammer/src/main/java/Question171.java
@@ -0,0 +1,21 @@
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ *
+ * @author : xiaomo
+ * github  : https://github.com/xiaomoinfo
+ * email   : xiaomo@xiaomo.info
+ * QQ      : 83387856
+ * Date    : 2017/11/22 17:39
+ * desc    :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question171 {
+    public static void main(String[] args) {
+        Boolean[] bool = new Boolean[2];
+        bool[0] = new Boolean(Boolean.parseBoolean("true"));
+        bool[1] = new Boolean(null);
+        System.out.println(bool[0] + " " + bool[1]);
+    }
+}
