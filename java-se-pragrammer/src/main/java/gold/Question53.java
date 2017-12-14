cebb93d93d359faeed7b44ce8136462ba50fbe02
xiaomo <xiaomo@xiaomo.info>
2017-12-14 13:16:14 +0800

    q55

---
+++ java-se-pragrammer/src/main/java/gold/Question53.java
@@ -0,0 +1,27 @@
+package gold;
+
+import gold.bean.X;
+import gold.bean.Y;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/12/14 10:41
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question53 {
+    public static void main(String[] args) {
+        X xRef = new Y();
+        Y yRef = (Y)xRef;
+        yRef.mY();
+        xRef.mX();
+    }
+}
---
+++ java-se-pragrammer/src/main/java/gold/bean/X.java
@@ -0,0 +1,21 @@
+package gold.bean;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/12/14 10:40
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class X {
+    public void mX() {
+        System.out.println("Xm11");
+    }
+}
