3d2f0d880430285dcb3736c5da75d18c1ce4a09e
xiaomo <xiaomo@xiaomo.info>
2017-12-18 20:01:17 +0800

    q14

---
+++ java-se-pragrammer/src/main/java/gold/Question14.java
@@ -0,0 +1,32 @@
+package gold;
+
+import gold.bean.Bird;
+import gold.bean.Penguin;
+
+import java.util.function.Supplier;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/12/18 16:51
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question14 {
+    public static void main(String[] args) {
+        fly(Bird::new);
+        fly(Penguin::new);
+
+    }
+
+    static void fly(Supplier<Bird> birdConsumer) {
+        birdConsumer.get().fly();
+    }
+}
--- java-se-pragrammer/src/main/java/gold/Question25.java
+++ java-se-pragrammer/src/main/java/gold/Question25.java
@@ -17,5 +17,17 @@ package gold;
 public class Question25 {
     public static void main(String[] args) {
         String color = "teal";
+        switch (color) {
+            case "RED":
+                System.out.println("Found Red");
+            case "Blue":
+                System.out.println("Found Blue");
+                break;
+            case "Teal":
+                System.out.println("Found Teal");
+                break;
+            default:
+                System.out.println("Found Default");
+        }
     }
 }
