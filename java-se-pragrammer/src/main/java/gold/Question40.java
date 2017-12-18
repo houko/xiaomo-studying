2e44ab7eaf05df385b1a33b33e7e6453164b3171
xiaomo <xiaomo@xiaomo.info>
2017-12-18 20:47:05 +0800

    q40

---
+++ java-se-pragrammer/src/main/java/gold/Question40.java
@@ -0,0 +1,34 @@
+package gold;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/12/18 20:42
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question40 {
+    public static void main(String[] args) throws IOException {
+        Stream<Path> stream = Files.walk(Paths.get(System.getProperty("user.home")));
+        stream.forEach(fName -> {
+            try {
+                System.out.println(fName + " ");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
