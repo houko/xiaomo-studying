6bc445f234e879702ed5c2674c89c1b0cb1d84b3
xiaomo <xiaomo@xiaomo.info>
2017-12-19 16:37:36 +0800

    q44

--- java-se-pragrammer/src/main/java/gold/Question44.java
+++ java-se-pragrammer/src/main/java/gold/Question44.java
@@ -21,8 +21,8 @@ import java.util.stream.Stream;
  */
 public class Question44 {
     public static void main(String[] args) {
-        List<Integer> list1 = Arrays.asList(10,20);
-        List<Integer> list2 = Arrays.asList(15,30);
-        Stream.of(list1,list2).flatMap(Collection::stream).forEach(s-> System.out.print(s + " "));
+        List<Integer> list1 = Arrays.asList(10, 20);
+        List<Integer> list2 = Arrays.asList(15, 30);
+        Stream.of(list1, list2).flatMap(Collection::stream).forEach(s -> System.out.print(s + " "));
     }
 }
