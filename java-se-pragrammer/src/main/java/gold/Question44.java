6a34a4a25a06e88752ed4571cd940228e05a99ce
xiaomo <xiaomo@xiaomo.info>
2017-12-19 08:57:04 +0800

    q44

---
+++ java-se-pragrammer/src/main/java/gold/Question44.java
@@ -0,0 +1,28 @@
+package gold;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * 把今天最好的表现当作明天最新的起点．．～
+ * いま 最高の表現 として 明日最新の始発．．～
+ * Today the best performance  as tomorrow newest starter!
+ * Created by IntelliJ IDEA.
+ * <p>
+ * author: xiaomo
+ * github: https://github.com/xiaomoinfo
+ * email : xiaomo@xiaomo.info
+ * QQ    : 83387856
+ * Date  : 2017/12/18 20:50
+ * desc  :
+ * Copyright(©) 2017 by xiaomo.
+ */
+public class Question44 {
+    public static void main(String[] args) {
+        List<Integer> list1 = Arrays.asList(10,20);
+        List<Integer> list2 = Arrays.asList(15,30);
+        Stream.of(list1,list2).flatMap(Collection::stream).forEach(s-> System.out.print(s + " "));
+    }
+}
