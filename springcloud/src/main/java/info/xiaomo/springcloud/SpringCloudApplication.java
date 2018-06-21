f1c9b1afe9d3c34b0223c4aebfaeea09add512e2
xiaomo <xiaomo@xiaomo.info>
2018-06-21 13:37:06 +0800

    first commit

---
+++ springcloud/src/main/java/info/xiaomo/springcloud/SpringCloudApplication.java
@@ -0,0 +1,12 @@
+package info.xiaomo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+}
