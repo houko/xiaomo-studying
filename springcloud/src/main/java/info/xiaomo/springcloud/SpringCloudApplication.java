005cafccee8ba860f94a8511f4c217bba7ba4b28
xiaomo <xiaomo@xiaomo.info>
2018-06-21 13:42:31 +0800

    添加启动入口显示

--- springcloud/src/main/java/info/xiaomo/springcloud/SpringCloudApplication.java
+++ springcloud/src/main/java/info/xiaomo/springcloud/SpringCloudApplication.java
@@ -2,11 +2,20 @@ package info.xiaomo.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCloudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudApplication.class, args);
+    }
+
+
+    @RequestMapping("/")
+    public String hello(){
+        return "xiaomo";
+    }
 }
