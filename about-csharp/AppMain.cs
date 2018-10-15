5f97edd1d66cb83e8fbb871efb1aa7a37c131748
hupeng <xiaomo@xiaomo.info>
2018-10-15 18:03:38 +0800

    面向对象属性

--- about-csharp/AppMain.cs
+++ about-csharp/AppMain.cs
@@ -5,7 +5,13 @@ namespace about_csharp
     {
         public static void Main(String[] args)
         {
-            Q3.Call(2018);
+            Q6 q6 = new Q6
+            {
+                Age = 5,
+                Name = "xiaomo"
+            };
+
+            Console.WriteLine("my name is {0},age is {1}", q6.Name, q6.Age);
         }
     }
 }
