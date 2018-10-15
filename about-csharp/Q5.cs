359fdd27896246b1f7f768bf3b7c889e57b55260
hupeng <xiaomo@xiaomo.info>
2018-10-15 17:47:08 +0800

    getmax and min

---
+++ about-csharp/Q5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace about_csharp
+{
+    /// <summary>
+    /// 获取数组中最大/最小的数
+    /// </summary>
+    public class Q5
+    {
+
+        /// <summary>
+        /// Gets the max.
+        /// </summary>
+        /// <returns>The max.</returns>
+        /// <param name="arr">Arr.</param>
+        public static int GetMax(params int[] arr)
+        {
+            int max = arr[0];
+            foreach (int i in arr)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+
+            return max;
+        }
+
+        /// <summary>
+        /// Gets the minimum.
+        /// </summary>
+        /// <returns>The minimum.</returns>
+        /// <param name="arr">Arr.</param>
+        public static int GetMin(params int[] arr)
+        {
+            int min = arr[0];
+            foreach (int i in arr)
+            {
+                if (i < min)
+                {
+                    min = i;
+                }
+            }
+
+            return min;
+        }
+    }
+}
---
+++ about-csharp/Q6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace about_csharp
+{
+    /// <summary>
+    /// 排序
+    /// </summary>
+    public static class Q6
+    {
+        public static int[] SelectSort(params int[] arr)
+        {
+            int temp = 0;
+            int size = arr.Length;
+            for (int i = 0; i < size; i++)
+            {
+                int k = i;
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j] < arr[k])
+                    {
+                        k = j;
+                    }
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+            return arr;
+        }
+    }
+}
