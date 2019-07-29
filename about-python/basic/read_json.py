5618a002910382f7c9f6ede441bab8c996b3d51e
kohou <hoh_ko@ricoh-japan.co.jp>
2019-07-29 16:14:43 +0900

    修改写法

--- about-python/basic/read_json.py
+++ about-python/basic/read_json.py
@@ -5,11 +5,11 @@ import requests
 # loads 读取json  dumps把python对象转成Json
 # load dump(l,f)
 
-ret = requests.get(url='http://httpbin.org/headers')
+ret = requests.get('http://httpbin.org/headers')
 res = json.loads(ret.content)
 print(res)
 
 print('\n')
 j = {'c': "abc", 'a': 'ddd'}
 
-print(json.dumps(j, sort_keys=True))
+print(json.dumps(j))
